dac8b0007928ba6653498bbaec3f99c33a496f49
pandaxel-pixel <pandaxel@gmail.com>
2025-09-12 12:30:11 -0400

    Remove weight and points from collector view

--- src/components/ItemsListView.tsx
+++ src/components/ItemsListView.tsx
@@ -72,10 +72,6 @@ export default function ItemsListView({ items, onAccept, onReject, onRefresh }: 
                       {item.title}
                     </h3>
                   </div>
-                  <div className="text-right">
-                    <div className="text-lg font-bold text-green-600">{item.points} pts</div>
-                    <div className="text-xs text-gray-500">Estimado por IA</div>
-                  </div>
                 </div>
 
                 <p className="text-gray-600 text-xs mb-3 line-clamp-2">
@@ -87,10 +83,6 @@ export default function ItemsListView({ items, onAccept, onReject, onRefresh }: 
                     <div className="flex items-center text-gray-600">
                       <MapPin className="w-3 h-3 mr-1 text-blue-500" />
                       <span>{item.location.address}</span>
-                    </div>
-                    <div className="flex items-center text-gray-500">
-                      <Weight className="w-3 h-3 mr-1" />
-                      {item.totalWeight}kg
                     </div>
                   </div>
 
